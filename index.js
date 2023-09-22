fbd0d26fc7d67cd97bfc5f5450448f46caa465ba
mauroprograma10 <programadorcerodiez@gmail.com>
2023-09-22 11:09:29 -0300

    i'll be back

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-let firstNumber = '';
-let secondNumber = '';
+let currentValue = '';
+let previousValue = '';
 let operator = null;
+
+const getButtons = document.querySelectorAll ('[data-number]');
+const getOperators = document.querySelectorAll ('[data-operator]')
+const clear = document.getElementById ('clear');
+let displayValue = document.getElementById ('showNum');
+
+
+getButtons.forEach ((button) => button.addEventListener ("click", () => appendNumber (button.textContent)));
+
+
+function appendNumber (num) {
+    if (displayValue.textContent === '0') {
+        resetScreen ()
+        displayValue.textContent += num
+    } 
+}
+
+function resetScreen () {
+    displayValue.textContent = ''
+}
+
 
 
 function add (a, b) {
